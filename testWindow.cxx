3dbc32f867585ffd9a2441f6f0af0b26f1d9a477
a-santamaria <chino.sebastian@gmail.com>
2016-05-30 18:14:12 -0500

    adding mlpack prediction in test window

--- testWindow.cxx
+++ testWindow.cxx
@@ -5,11 +5,14 @@
 #include <fstream>
 #include <sstream>
 
+#include "mlpack/core.hpp"
+#include "mlpack/methods/logistic_regression/logistic_regression.hpp"
 #include "descriptorLBPH.h"
 #include "pedestrianRecognizer.h"
 
 using namespace cv;
-
+using namespace mlpack;
+using namespace mlpack::regression;
 
 int main(int argc, char** argv ) {
 
@@ -28,9 +31,25 @@ int main(int argc, char** argv ) {
         return -1;
     }
 
-    PedestrianRecognizer model(modelFile);
+    // PedestrianRecognizer model(modelFile);
 
-    double prob = model.pedestrianProbability(img);
+    // double prob = model.pedestrianProbability(img);
+
+    LogisticRegression<> model(0,0);
+    data::Load(modelFile, "logReg_model", model);
+    std::cout << "cargo" << std::endl;
+    arma::mat predictors(DescriptorLBPH::desSize, 1);
+    arma::Row<size_t> responses(1);
+
+    DescriptorLBPH dlbp = DescriptorLBPH(img);
+    for(int j = 0; j < DescriptorLBPH::desSize; j++) {
+        //TODO revisar que si sea en este orden
+        predictors(j, 0) = dlbp.descriptor[j];
+    }
+    //std::cout << "predictors " << predictors << std::endl;
+    model.Predict(predictors, responses);
+    double prob = responses(0);
+    std::cout << "responses " << responses << std::endl;
     std::cout << "pedestrian probability: " << prob << std::endl;
     // Create a window for display img.
     namedWindow( "Display window", WINDOW_AUTOSIZE );
